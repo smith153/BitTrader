cc4819e206e64d3341b52ac8201b89c7022dd1d6
Samuel Smith <leon36@gmail.com>
2014-09-18 16:56:02 -0500

    new version of load_btce.pl

--- bin/load_btce.pl
+++ bin/load_btce.pl
@@ -4,13 +4,26 @@ use strict;
 use POSIX qw(setsid);
 use DBI;
 use Finance::btce;
-use BitTrader::Config;
+use Time::HiRes qw/time sleep/;
+
 
 #monitor tickers on btc-e.com, 
 #insert values into DB for later testing
+#runs as daemon if @ARGV > 0
+#creates one child perl symbol
 
-
+my @pids;
 my $cfg = BitTrader::Config->new();
+
+#kill pids on term
+$SIG{TERM} = sub{
+	warn get_time() . "Killing child processes\n";
+	kill 'TERM', @pids;
+	warn get_time() . "Exiting\n";
+	exit;
+};
+
+
 
 sub mysql_connect
 {
@@ -19,7 +32,7 @@ sub mysql_connect
 	my $user = "btce";
 	my $pw = $cfg->get_var("db_pass");
 	my $dsn = "dbi:Pg:database=$database;host=$host";
-	return  DBI->connect($dsn, $user, $pw) || die "Unable to connect: $DBI::errstr\n";
+	return  DBI->connect_cached($dsn, $user, $pw) || die "Unable to connect: $DBI::errstr\n";
 }
 
 sub get_time
@@ -38,69 +51,95 @@ sub daemonize
 	setsid or die "Can't start a new session: $!";
 	umask 0;
 	close $_ or die $! for @fh_unused;
-	open STDOUT, ">>$ENV{HOME}/btce/load_btce.log" or die "Can't write to log $!";
-	open STDERR, ">>$ENV{HOME}/btce/load_btce.log" or die "Can't write to log $!";
+	open STDOUT, ">>$ENV{HOME}load_btce.log" or die "Can't write to log $!";
+	open STDERR, ">>$ENV{HOME}load_btce.log" or die "Can't write to log $!";
 }
 
 
-sub insert
-{
-	my ($sym, $btce,$rate) = @_;
-	my $last;
-	my $dbh = mysql_connect();
-	my $ref = $btce->getTicker($sym . "_usd");
 
-	if( not defined $ref or not defined $ref->{last} or not $ref->{last} > 0){
-		warn get_time() . "Last not defined.\n";
-		warn get_time() . "Last not > 0. Last is: $ref->{last}\n" if( defined $ref->{last});
-		exit();
+sub insert2
+{
+	my ($sym, $btce) = @_;
+	my $last;
+	my $dbh;
+	my $qh;
+	my $ref;
+	my $count = 0;
+	my $sleep_time;
+	my $next_wakeup = time() + 10;
+
+
+	while(1){
+		$dbh = mysql_connect();
+
+		unless($dbh){
+			warn get_time() . "Coudln't connect to DB\n";
+			sleep 1;
+			next;
+		}
+		$qh = $dbh->prepare_cached("INSERT INTO btce_track (price,symbol,rate) VALUES(?,?,?)");
+		unless($qh){
+			warn get_time() .  "Couldn't prepare query\n";
+			sleep 1;
+			next;
+		}
+
+		$ref = $btce->getTicker($sym . "_usd");
+		if( not defined $ref or not defined $ref->{last} or not $ref->{last} > 0){
+			warn get_time() . "Last not defined.\n";
+			warn get_time() . "Last not > 0. Last is: $ref->{last}\n" if( defined $ref->{last});
+			next;
+		}
+
+		$last = $ref->{last};
+		print get_time() . "Inserting for $sym price: $last\n";
+
+		$qh->execute($last,$sym,10) or print "Coudn't insert for $sym\n";
+		if($count == 30){
+			$count = 0;
+			$qh->execute($last,$sym,300) or print "Coudn't insert for $sym and rate 300\n";
+		}
+		$count++;
+		STDOUT->flush();
+
+		$sleep_time = $next_wakeup - time();
+		if($sleep_time <= 0){
+			$next_wakeup = time() + 10;
+			next;
+		}
+		sleep $sleep_time;
+		$next_wakeup += 10;
 	}
-	$last = $ref->{last};
-	print get_time() . "Inserting for $sym price: $last\n";
-	$dbh->do("INSERT INTO btce_track (price,symbol,rate) VALUES(?,?,?)",undef, $last,$sym,10) or print "Coudn't insert for $sym\n";
-	if($rate != 10){
-		$dbh->do("INSERT INTO btce_track (price,symbol,rate) VALUES(?,?,?)",undef, $last,$sym,300) or print "Coudn't insert for $sym\n";
-	}
-	$dbh->disconnect();
-	exit();
 
 }
 
 ###########################################################################
 
 daemonize() if @ARGV;
-$SIG{CHLD} = "IGNORE";
 
-my $i = 0;
-my $rate;
-#my @symbols = qw/btc ltc nmc nvc ppc/;
-my @symbols = qw/btc ltc/;
+my @symbols = qw/btc ltc nmc nvc ppc/;
 my $btce = Finance::btce->new({
 	apikey => $cfg->get_var("apikey"),
 	secret => $cfg->get_var("apisecret"),
 });
 
 
-while(1){
-	if($i != 30){
-		$rate = 10;
-		$i++;
+while( my $sym = pop(@symbols)){
+	my $pid = fork();
+	die "Fork failed\n" if($pid == -1);
+
+	if($pid == 0){ #in child
+		$SIG{TERM} = sub { exit; };
+		insert2($sym,$btce);
 	}
 	else{
-		$rate = 310;
-		$i = 0;
+		push(@pids, $pid);
 	}
 
-	foreach my $sym (@symbols){
-		my $pid = fork();
-		die "fork failed" if $pid == -1;
-		if($pid == 0){ #in child
-			insert($sym,$btce,$rate);
-		}
-	}
+}
 
-	sleep 10;
-	STDOUT->flush();
+while( my $pid = pop(@pids)){
+	waitpid($pid,0);	
 }
 
 
