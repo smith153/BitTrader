b515c93ede266066adb86f48c218345dff33942a
Samuel Smith <leon36@gmail.com>
2014-10-18 16:48:51 -0500

    use Math::Ewa

--- lib/BitTrader/Indicator/Ma.pm
+++ lib/BitTrader/Indicator/Ma.pm
@@ -1,68 +1,63 @@
 package BitTrader::Indicator::Ma;
 use namespace::autoclean;
 use Moose;
-with 'BitTrader::Ewa';
+use Math::Ewa;
 with 'BitTrader::Indicator';
 
-has 'avg' => (	is => 'ro', isa => 'Num', writer => '_set_avg',);
-has 'avg_slow' => (	is => 'ro', isa => 'Num', writer => '_set_avg_slow',);
-has 'avg_slower' => (	is => 'ro', isa => 'Num', writer => '_set_avg_slower',);
-has 'avg_slowest' => (	is => 'ro', isa => 'Num', writer => '_set_avg_slowest',);
+has 'avg' => (	is => 'ro', isa => 'Math::Ewa',
+		builder => sub {return Math::Ewa->new(alpha => 1/8)},
+);
+has 'avg_slow' => (	is => 'ro', isa => 'Math::Ewa',
+		builder => sub {return Math::Ewa->new(alpha => 1/16)},
+);
+has 'avg_slower' => (	is => 'ro', isa => 'Math::Ewa',
+		builder => sub {return Math::Ewa->new(alpha => 1/32)},
+);
+has 'avg_slowest' => (	is => 'ro', isa => 'Math::Ewa',
+		builder => sub {return Math::Ewa->new(alpha => 1/128)},
+);
 
 
 sub _update_indicator
 {
-  my ($self, $cur) = @_;
-  my $avg;
+	my ($self, $cur) = @_;
 
-  $avg = $self->avg();
-  $avg = $self->ewa($cur,$avg,1/8);
-  $self->_set_avg($avg);
-
-  $avg = $self->avg_slow();
-  $avg = $self->ewa($cur,$avg,1/32);
-  $self->_set_avg_slow($avg);
-
-  $avg = $self->avg_slower();
-  $avg = $self->ewa($cur,$avg,1/64);
-  $self->_set_avg_slower($avg);
-
-  $avg = $self->avg_slowest();
-  $avg = $self->ewa($cur,$avg,1/128);
-  $self->_set_avg_slowest($avg);
+	$self->avg->ewa($cur);
+	$self->avg_slow->ewa($cur);
+	$self->avg_slower->ewa($cur);
+	$self->avg_slowest->ewa($cur);
 
 }
 
 sub should_buy
 {
-  my $self = shift();
-  my $l    = $self->avg();
-  my $ll   = $self->avg_slow();
-  my $lll  = $self->avg_slower();
-  my $llll = $self->avg_slowest();
+	my $self = shift();
+	my $l    = $self->avg->last_avg();
+	my $ll   = $self->avg_slow->last_avg();
+	my $lll  = $self->avg_slower->last_avg();
+	my $llll = $self->avg_slowest->last_avg();
 
-  if($l > $ll and $ll > $lll and $lll > $llll){ #market trending
-	return 1;
-  }
-  return 0;
+	if($l > $ll and $ll > $lll and $lll > $llll){ #market trending
+		return 1;
+	}
+	return 0;
 
 }
 
 sub should_sell
 {
-  my $self = shift();
-  my $l    = $self->avg();
-  my $ll   = $self->avg_slow();
-  my $lll  = $self->avg_slower();
-  my $llll = $self->avg_slowest();
+	my $self = shift();
+	my $l    = $self->avg->last_avg();
+	my $ll   = $self->avg_slow->last_avg();
+	my $lll  = $self->avg_slower->last_avg();
+	my $llll = $self->avg_slowest->last_avg();
 
-  if($llll > $lll and $lll > $ll and $ll > $l){#market falling
-	return 1;
-  }
-  return 0;
+	if($llll > $lll and $lll > $ll and $ll > $l){#market falling
+		return 1;
+	}
+	return 0;
 
 }
-
 
 
 __PACKAGE__->meta->make_immutable;
