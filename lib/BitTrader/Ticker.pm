8ee9068d80bee60f8f6413a09754f8d364c69236
Samuel Smith <esaym@cpan.org>
2015-06-18 14:23:54 -0500

    Re-tab
    ugh

--- lib/BitTrader/Ticker.pm
+++ lib/BitTrader/Ticker.pm
@@ -38,82 +38,82 @@ has '_cfg' => (
 
 sub BUILD
 {
-  my $self = shift();
+	my $self = shift();
 
 
-  foreach my $indicator (split( /,/,$self->_cfg->get_var('indicators')) ){
-	my $module = "BitTrader::Indicator::$indicator";
-	eval "require $module" or die $@;
-	$self->_add_indicator($indicator => $module->new() );
-  }
+	foreach my $indicator (split( /,/,$self->_cfg->get_var('indicators')) ){
+		my $module = "BitTrader::Indicator::$indicator";
+		eval "require $module" or die $@;
+		$self->_add_indicator($indicator => $module->new() );
+	}
 
-  $self->get_indicator();
+	$self->get_indicator();
 }
 
 sub ticker_status
 { 
-  my $self = shift();
-  my $txt = "\n\tSym: ${\$self->symbol()}\n\tAmount: ${\$self->amount()}\n\tCurr price: ${\$self->price()}\n\tAverages: " .
+	my $self = shift();
+	my $txt = "\n\tSym: ${\$self->symbol()}\n\tAmount: ${\$self->amount()}\n\tCurr price: ${\$self->price()}\n\tAverages: " .
 		"${\$self->avg()} ${\$self->avg_slow()} ${\$self->avg_slower()} ${\$self->avg_slowest()}\n\tK: ${\$self->k()} D: ${\$self->d()}" .
 		"\n\tK_fast: ${\$self->k_fast()} D_fast: ${\$self->d_fast()}\n\tTotal value: \$" . int($self->amount() * $self->price() ). "\n\tLast_ac: ${\$self->last_action()}\n\n";
-  return $txt;
+	return $txt;
 }
 
 
 sub get_indicator
 {
-  my $self = shift();
-  my $txt;
-  my $old = ( $self->cur_indicator() ) ?  (split( /\:\:/, blessed $self->cur_indicator()))[2] : "";
-  my $new = $self->_cfg->get_var('cur_indicator');
+	my $self = shift();
+	my $txt;
+	my $old = ( $self->cur_indicator() ) ?  (split( /\:\:/, blessed $self->cur_indicator()))[2] : "";
+	my $new = $self->_cfg->get_var('cur_indicator');
 
-  return if ($old eq $new);
+	return if ($old eq $new);
 
-  $self->_set_indicator($self->_get_indicator($new));
-
-  if(not $self->_indicator_valid($new) ){
-	my $indicators = join(' ', $self->_list_indicators() );
-	$self->log->error("Indicator does not match one of $indicators!");
-  }
-  else{
 	$self->_set_indicator($self->_get_indicator($new));
-  }
 
-  $self->log->info("Indicator changed. Was $old, now is $new.");
+	if(not $self->_indicator_valid($new) ){
+		my $indicators = join(' ', $self->_list_indicators() );
+		$self->log->error("Indicator does not match one of $indicators!");
+	}
+	else{
+		$self->_set_indicator($self->_get_indicator($new));
+	}
+
+	$self->log->info("Indicator changed. Was $old, now is $new.");
 }
 
 sub _update_indicators
 {
-  my ($self, $cur) = @_;
+	my ($self, $cur) = @_;
 
-  foreach my $indicator ($self->_list_indicators()){
-	$self->_get_indicator($indicator)->set_price($cur);
-  }
+	foreach my $indicator ($self->_list_indicators()){
+		$self->_get_indicator($indicator)->set_price($cur);
+	}
 }
 
 
 sub should_buy
 {
-  my $self = shift();
-  if($self->cur_indicator()->should_buy and $self->last_action ne 'buy'){
-	return 1;
-  }
-  if(-e "/tmp/buy"){
-	return 1;
-  }
-  return 0;
+	my $self = shift();
+	if($self->cur_indicator()->should_buy and $self->last_action ne 'buy'){
+		return 1;
+	}
+	if(-e "/tmp/buy"){
+		return 1;
+	}
+	return 0;
 }
 
 sub should_sell
 {
-  my $self = shift();
-  if($self->cur_indicator()->should_sell and $self->last_action ne 'sell'){
-	return 1;
-  }
-  if(-e "/tmp/sell"){
-	return 1;
-  }
-  return 0;
+	my $self = shift();
+	if($self->cur_indicator()->should_sell and $self->last_action ne 'sell'){
+		return 1;
+	}
+	if(-e "/tmp/sell"){
+		return 1;
+	}
+	return 0;
 }
 
 
